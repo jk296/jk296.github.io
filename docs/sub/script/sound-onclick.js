c0ccc14400366123807664f08df8a80bdbb6d88e
jk296 <71560379+jk296@users.noreply.github.com>
2022-12-24 15:19:11 +0000

    Add files via upload

---
+++ docs/sub/script/sound-onclick.js
@@ -0,0 +1,30 @@
+
+function playclip_01() {
+if (document.all){document.all.sound.src = "click.mp3";}
+
+else {{var audio = document.getElementById("Johnnykoon_116");audio.play();}}
+}
+
+function playclip_02() {
+if (document.all){document.all.sound.src = "click.mp3";}
+
+else {{var audio = document.getElementById("Johnnykoon_115");audio.play();}}
+}
+
+function playclip_03() {
+if (document.all){document.all.sound.src = "click.mp3";}
+
+else {{var audio = document.getElementById("Johnnykoon_114");audio.play();}}
+}
+
+function playclip_04() {
+if (document.all){document.all.sound.src = "click.mp3";}
+
+else {{var audio = document.getElementById("Johnnykoon_113");audio.play();}}
+}
+
+function playclip_05() {
+if (document.all){document.all.sound.src = "click.mp3";}
+
+else {{var audio = document.getElementById("Johnnykoon_112");audio.play();}}
+}
